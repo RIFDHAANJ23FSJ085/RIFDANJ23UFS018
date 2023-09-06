cbbf6164ba39ff1465d3917cd954f945037455e2
Rifdhan <138508786+RIFDHAANJ23FSJ085@users.noreply.github.com>
2023-09-06 15:01:07 +0530

    Add files via upload

---
+++ Lab21/simple_plugin.js
@@ -0,0 +1,15 @@
+// (function($){
+
+// }(jQuery));
+
+// (function($){
+//  jQuery.prototype.simplePlugin=function(){
+//     alert("Cool plugin");
+//  };   
+// }(jQuery));
+
+(function($){
+    $.fn.simplePlugin=function(){
+        alert("Cool Plugin");
+    };
+}(jQuery));
